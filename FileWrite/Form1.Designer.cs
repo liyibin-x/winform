c9141f9823d5d5de0a40f60037bba034d0822022
liyibin <113887935+liyibin-x@users.noreply.github.com>
2025-04-23 21:13:11 +0800

    FileTool
    记事本

---
+++ FileWrite/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace FileWrite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonOpen = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonSave = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonDelete = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonNew = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonNewFile = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabelMake = new System.Windows.Forms.ToolStripLabel();
+            this.textBoxNote = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolStripButtonElseSave = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonOpen,
+            this.toolStripButtonSave,
+            this.toolStripButtonElseSave,
+            this.toolStripButtonDelete,
+            this.toolStripButtonNew,
+            this.toolStripButtonNewFile,
+            this.toolStripLabelMake});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(565, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonOpen
+            // 
+            this.toolStripButtonOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonOpen.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonOpen.Image")));
+            this.toolStripButtonOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonOpen.Name = "toolStripButtonOpen";
+            this.toolStripButtonOpen.Size = new System.Drawing.Size(43, 24);
+            this.toolStripButtonOpen.Text = "打开";
+            this.toolStripButtonOpen.Click += new System.EventHandler(this.toolStripButtonOpen_Click);
+            // 
+            // toolStripButtonSave
+            // 
+            this.toolStripButtonSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonSave.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonSave.Image")));
+            this.toolStripButtonSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonSave.Name = "toolStripButtonSave";
+            this.toolStripButtonSave.Size = new System.Drawing.Size(43, 24);
+            this.toolStripButtonSave.Text = "保存";
+            this.toolStripButtonSave.Click += new System.EventHandler(this.toolStripButtonSave_Click);
+            // 
+            // toolStripButtonDelete
+            // 
+            this.toolStripButtonDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonDelete.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonDelete.Image")));
+            this.toolStripButtonDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonDelete.Name = "toolStripButtonDelete";
+            this.toolStripButtonDelete.Size = new System.Drawing.Size(43, 24);
+            this.toolStripButtonDelete.Text = "删除";
+            this.toolStripButtonDelete.Click += new System.EventHandler(this.toolStripButtonDelete_Click);
+            // 
+            // toolStripButtonNew
+            // 
+            this.toolStripButtonNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonNew.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonNew.Image")));
+            this.toolStripButtonNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonNew.Name = "toolStripButtonNew";
+            this.toolStripButtonNew.Size = new System.Drawing.Size(43, 24);
+            this.toolStripButtonNew.Text = "新建";
+            this.toolStripButtonNew.Click += new System.EventHandler(this.toolStripButtonNew_Click);
+            // 
+            // toolStripButtonNewFile
+            // 
+            this.toolStripButtonNewFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonNewFile.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonNewFile.Image")));
+            this.toolStripButtonNewFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonNewFile.Name = "toolStripButtonNewFile";
+            this.toolStripButtonNewFile.Size = new System.Drawing.Size(88, 24);
+            this.toolStripButtonNewFile.Text = "新建文件夹";
+            this.toolStripButtonNewFile.Click += new System.EventHandler(this.toolStripButtonNewFile_Click);
+            // 
+            // toolStripLabelMake
+            // 
+            this.toolStripLabelMake.Name = "toolStripLabelMake";
+            this.toolStripLabelMake.Size = new System.Drawing.Size(24, 24);
+            this.toolStripLabelMake.Text = "。";
+            this.toolStripLabelMake.Click += new System.EventHandler(this.toolStripLabelMake_Click);
+            // 
+            // textBoxNote
+            // 
+            this.textBoxNote.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxNote.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxNote.Location = new System.Drawing.Point(0, 27);
+            this.textBoxNote.Multiline = true;
+            this.textBoxNote.Name = "textBoxNote";
+            this.textBoxNote.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxNote.Size = new System.Drawing.Size(565, 419);
+            this.textBoxNote.TabIndex = 1;
+            this.textBoxNote.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // toolStripButtonElseSave
+            // 
+            this.toolStripButtonElseSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonElseSave.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonElseSave.Image")));
+            this.toolStripButtonElseSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonElseSave.Name = "toolStripButtonElseSave";
+            this.toolStripButtonElseSave.Size = new System.Drawing.Size(58, 24);
+            this.toolStripButtonElseSave.Text = "另存为";
+            this.toolStripButtonElseSave.Click += new System.EventHandler(this.toolStripButtonElseSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 446);
+            this.Controls.Add(this.textBoxNote);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonOpen;
+        private System.Windows.Forms.ToolStripButton toolStripButtonSave;
+        private System.Windows.Forms.ToolStripButton toolStripButtonDelete;
+        private System.Windows.Forms.ToolStripButton toolStripButtonNew;
+        private System.Windows.Forms.ToolStripButton toolStripButtonNewFile;
+        private System.Windows.Forms.TextBox textBoxNote;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabelMake;
+        private System.Windows.Forms.ToolStripButton toolStripButtonElseSave;
+    }
+}
+
---
+++ FileWrite/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//
+using System.Drawing.Text;
+using System.Collections;
+using System.IO;
+
+
+namespace FileWrite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //窗口改变事件
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //在Label写入*
+            toolStripLabelMake.Text = "*";
+        }
+        //打开文本文件
+        private void toolStripButtonOpen_Click(object sender, EventArgs e)
+        {
+            //设置文档类型
+            openFileDialog1.Filter = ("文本文档(*.txt)|*.txt");
+            //判断打开按键是否按下
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //获取文档路径
+                string path = openFileDialog1.FileName;
+                //通用编码UTF8
+                StreamReader sr = new StreamReader(path, Encoding.UTF8);
+                //读取文档的数据流
+                string text = sr.ReadToEnd();
+                //输出文档
+                textBoxNote.Text = text;
+                //将路径写入窗口text
+                this.Text = path;
+                //清空计号
+                toolStripLabelMake.Text = "";
+                //清理缓存
+                sr.Close();
+
+            }
+           
+        }
+        //新建文件夹
+        private void toolStripButtonNewFile_Click(object sender, EventArgs e)
+        {
+            //判断新建文件夹按键
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //读取文件夹地址
+                string path = folderBrowserDialog1.SelectedPath;
+                //新建文件夹
+                Directory.CreateDirectory(path);
+            }
+            
+        }
+        //保存文档文件
+        private void toolStripButtonSave_Click(object sender, EventArgs e)
+        {
+            //判断文本内容是否为空
+            if(textBoxNote.Text.Trim() != "")
+            {   //判断窗口text是否为空
+                if (this.Text == "")
+                {   //文档类型
+                    saveFileDialog1.Filter = ("文本文档(*.txt)|*.txt");
+                    //判断是否按下
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {   //获取保存地址
+                        string path = saveFileDialog1.FileName;
+                        //保存到指定路径
+                        StreamWriter sw = new StreamWriter(path, false);
+                        sw.WriteLine(textBoxNote.Text.Trim());
+                        this.Text = path;
+                        toolStripLabelMake.Text = "";
+                        sw.Close();
+
+                    }
+                }
+                //已经打开的文档直接保存
+                else
+                {  
+                    string path = saveFileDialog1.FileName;
+                    path = this.Text;
+                    StreamWriter sw = new StreamWriter(path, false);
+                    sw.WriteLine(textBoxNote.Text.Trim());
+                    toolStripLabelMake.Text = "";
+                    sw.Close();
+                }
+                
+               
+            }
+            
+           
+        }
+        //删除文件文档
+        private void toolStripButtonDelete_Click(object sender, EventArgs e)
+        {
+           
+            openFileDialog1.Filter = ("文本文档(*.txt)|*.txt");
+            openFileDialog1.Title = "删除";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                File.Delete(path);
+            
+            }
+        }
+        //新建文件文档
+        private void toolStripButtonNew_Click(object sender, EventArgs e)
+        {
+            textBoxNote.Text = "";
+            toolStripLabelMake.Text = "";
+            this.Text = "";
+            
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //未保存退出判断
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (toolStripLabelMake.Text == "*")
+            {
+                DialogResult dr = MessageBox.Show("文本未保存，确认退出吗？", "信息质询",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    this.Dispose();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+
+            }
+        }
+
+        private void toolStripLabelMake_Click(object sender, EventArgs e)
+        {
+
+        }
+        //另存为
+        private void toolStripButtonElseSave_Click(object sender, EventArgs e)
+        {
+            if (textBoxNote.Text.Trim() != "")
+            {
+                saveFileDialog1.Filter = ("文本文档(*.txt)|*.txt");
+                //判断是否按下
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {   //获取保存地址
+                    string path = saveFileDialog1.FileName;
+                    //保存到指定路径
+                    StreamWriter sw = new StreamWriter(path, false);
+                    sw.WriteLine(textBoxNote.Text.Trim());
+                    this.Text = path;
+                    toolStripLabelMake.Text = "";
+                    sw.Close();
+
+                }
+            }
+        }
+    }
+}
